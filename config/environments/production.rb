81f00824f7ac390d9be398dc6df60f879e4391ae
kaye <kaaboaye@gmail.com>
2017-06-09 15:37:06 +0200

    :P

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
