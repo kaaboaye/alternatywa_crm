5a907315ec6f869b2bde91cf0a01db8fba32545f
kaye <kaaboaye@gmail.com>
2017-06-10 18:58:00 +0200

    refa:

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -115,6 +115,12 @@ class LessonsController < ApplicationController
     end
   end
 
+  def destroy
+    lesson = Lesson.find params[:id]
+    lesson.destroy
+    redirect_to lessons_path
+  end
+
 private
   def lesson_params
     params.permit :giving_member_id, :taking_member_id, :lesson_subject_id,
